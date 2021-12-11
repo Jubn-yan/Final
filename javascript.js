77ea5b97bd4980bcc2fc37a978d732e3b79e39af
Jubn-yan <jubn@mail.uc.edu>
2021-12-12 00:41:01 +0800

    create onlick function  for start.html

--- javascript.js
+++ javascript.js
@@ -9,3 +9,39 @@
   var interval,flag=1,sum=0; 
   
   var speedX = -1,speedY = -1;
+
+  btn.onclick = function() {
+    clearInterval(interval);
+    flag=0;
+    interval =  setInterval(function(){   
+      ballLeft += speedX;
+      ballTop += speedY;
+
+      if(ballLeft>=0 && ballLeft<=485){
+        ball.style.left = ballLeft + "px";
+      }
+      if(ballTop>=0 && ballTop<=385){
+        ball.style.top = ballTop + "px";
+      }
+
+      if(ballLeft<0|| ballLeft>485 ){
+        speedX = -speedX;  
+      }else if(ballTop<0){
+        speedY = -speedY; 
+      }  
+     
+      if((ballTop+15) >= boardTop && ballLeft>=boardLeft && ballLeft <= (boardLeft+50)){
+        speedX = -speedX; 
+        speedY = -speedY;
+        sum+=5;
+        score.innerHTML = sum;
+
+      }
+      if(ballTop>385 ){
+        alert("Game over please refresh to restart!");
+        clearInterval(interval);
+      }
+    },5) }
+   
+ 
+ 
